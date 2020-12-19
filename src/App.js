aa52f7137cdaf03b6cd1651ef5f5ce0441c87006
reebhu12 <reebhugoku@gmail.com>
2020-12-19 18:36:29 +0530

    Monster Rolodex

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(usersResponse => this.setState({ monsters: usersResponse }));
+  }
+  render() {
+    const { monsters, searchField } = this.state;
+    const filterMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    return (
+      <div className="App">
+        <h1> Monster Rolodex </h1>
+        <SearchBox placeholder='search monster' 
+        handleChange = {e => this.setState({searchField : e.target.value})}/>
+        <CardList monsters={filterMonsters}>
+        </CardList>
+      </div>);
+  }
+}
+
+export default App;
